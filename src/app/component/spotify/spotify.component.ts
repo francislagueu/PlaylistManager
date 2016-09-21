dcfd7c3e386763622cc9e89202fd5ad9083b1902
francislagueu <lagson6@yahoo.fr>
2016-09-20 22:54:18 -0700

    added a couple line in the youtube and spotify component file

--- src/app/component/spotify/spotify.component.ts
+++ src/app/component/spotify/spotify.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import {SpotifyService} from '../../services/spotify.service';
 
 @Component({
   moduleId: module.id,
   selector: 'spotify',
-  templateUrl: './component/spotify/spotify.component.html'
+  templateUrl: './component/spotify/spotify.component.html',
+  providers: [SpotifyService]
 })
 export class SpotifyComponent {
-
+    constructor(){
+      console.log('Spotify Service Init.....');
+    }
 }
--- src/app/component/youtube/youtube.component.ts
+++ src/app/component/youtube/youtube.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import {YoutubeService} from '../../services/youtube.service';
 
 @Component({
   moduleId: module.id,
   selector: 'youtube',
-  templateUrl: './component/youtube/youtube.component.html'
+  templateUrl: './component/youtube/youtube.component.html',
+  providers: [YoutubeService]
 })
 export class YoutubeComponent {
-
+    constructor(){
+      console.log('Youtube Service Init....');
+    }
 }
