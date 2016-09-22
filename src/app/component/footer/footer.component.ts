9ecd3fd06d03cc73c70a7b04531dc38ab9af3f2d
francislagueu <lagson6@yahoo.fr>
2016-09-21 17:33:02 -0700

    Deleted some line from the conflict merge

--- src/app/component/footer/footer.component.ts
+++ src/app/component/footer/footer.component.ts
@@ -1,14 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  //moduleId: module.id,
+  moduleId: module.id,
   selector: 'footer',
-<<<<<<< HEAD
   templateUrl: 'footer.component.html',
   styleUrls: ['footer.component.css']
-=======
-  templateUrl: './footer.component.html'
->>>>>>> 4c3e0fdd25fba88004bef8cd11c60e09b8cf79df
 })
 export class FooterComponent {
 
