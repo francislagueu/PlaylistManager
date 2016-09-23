11b063a944200b51ef6ef0a6a3ac2d335f436b96
Francis <lagson6@yahoo.fr>
2016-09-22 19:11:05 -0700

    Added the search folder and all its files

---
+++ src/app/component/search/search.component.ts
@@ -0,0 +1,12 @@
+
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'search',
+  templateUrl: 'search.component.html',
+  styleUrls: ['search.component.css']
+})
+export class SearchComponent {
+
+}
