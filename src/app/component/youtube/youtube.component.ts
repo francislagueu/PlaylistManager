83f99a18aed380d941ffe9d8a3c14783b7290814
bryanherrera72 <bryan.herrera72@gmail.com>
2016-09-29 16:55:33 -0700

    Added the gapi script which possibly works?

--- src/app/component/youtube/youtube.component.ts
+++ src/app/component/youtube/youtube.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import {YoutubeService} from '../../services/youtube.service';
-
+declare var gapi:any;
 @Component({
    moduleId: module.id,
   selector: 'youtube',
@@ -9,7 +9,23 @@ import {YoutubeService} from '../../services/youtube.service';
   providers: [YoutubeService]
 })
 export class YoutubeComponent {
+	clientId:"788672495349-mmdrb1o2krhg9ll45mj750rah5fl57pe.apps.googleusercontent.com";
+	apiKey="smiling-castle-143717";
+	scopes = "https://www.googleapis.com/auth/youtube";
+	gapi;
+	auth2;
     constructor(){
       console.log('Youtube Service Init....');
+      gapi.load('client:auth2',this.initAuth());
+    }
+    ngAfterViewInit(){
+    	this.initAuth();
+    	console.log("gapi", gapi);
+    }
+    public initAuth(){
+
+    }
+    public onSignIn(googleUser){
+
     }
 }
