7ccaa8575f326d2944f5c44fec19a134bc255bf0
Francis <lagson6@yahoo.fr>
2016-10-07 10:25:31 -0700

    created the login component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import {MaterialModule} from '@angular/material';
 
 import { AppComponent } from './app.component';
 import {NavbarComponent} from './component/navbar/navbar.component';
+import {LoginComponent} from './component/login/login.component';
 import {FooterComponent} from './component/footer/footer.component';
 import {SpotifyComponent} from './component/spotify/spotify.component';
 import {YoutubeComponent} from './component/youtube/youtube.component';
@@ -15,12 +17,14 @@ import {YoutubeComponent} from './component/youtube/youtube.component';
     NavbarComponent,
     FooterComponent,
     SpotifyComponent,
-    YoutubeComponent
+    YoutubeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MaterialModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
